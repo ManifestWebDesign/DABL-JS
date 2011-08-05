b2a10cb7915d7b5c9a96cbea91b314e4747024e4
Aaron Fellin <aaron@manifestwebdesign.com>
2011-08-05 14:22:18 -0700

    .

--- minify.sh
+++ minify.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 /bin/rm dabl.min.js 2>/dev/null
-for i in "src/*.js" ; do java -jar yuicompressor-2.4.6.jar --type js $i ; done >dabl.min.js
+for i in src/*.js ; do
+	echo '/* '$(basename $i)' */' >> dabl.min.js
+	java -jar yuicompressor-2.4.6.jar --type js $i >>dabl.min.js
+	echo >> dabl.min.js
+	echo >> dabl.min.js
+done
