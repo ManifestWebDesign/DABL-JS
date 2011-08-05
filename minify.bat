f4982d66928a4dfc74f50cb9fad64fa10c844405
Dan Blaisdell <dan@manifestwebdesign.com>
2011-08-05 15:46:01 -0700

    new minify.bat

--- minify.bat
+++ minify.bat
@@ -1,3 +1,8 @@
 @ECHO OFF & SETLOCAL
 DEL dabl.min.js
-for %%i IN (src\*.js) DO java -jar yuicompressor-2.4.6.jar --type js %%i>>dabl.min.js
+for %%i IN (src\*.js) DO (
+	ECHO /* %%~ni.js */>> dabl.min.js
+	java -jar yuicompressor-2.4.6.jar --type js %%i>>dabl.min.js
+	ECHO.>> dabl.min.js
+	ECHO.>> dabl.min.js
+)
